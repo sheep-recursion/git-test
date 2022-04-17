677916db180f49e577e9b87b8f39939e3ee47015
yangwise <102008576+yangwise@users.noreply.github.com>
2022-04-17 14:43:55 +0800

    Update GitTest.java

--- src/main/java/com/aiguigu/git/GitTest.java
+++ src/main/java/com/aiguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hot-fix text");
         System.out.println("pull text");
+        System.out.println("gitee text");
     }
 }
